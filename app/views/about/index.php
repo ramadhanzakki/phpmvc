dc69b04bf3189500a34ecfead9d590e85b28d6e4
ramadhanzakki <ramadhanzakki506@gmail.com>
2026-04-21 14:25:30 +0700

    feat: added photo profile on about/index

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>About Me</h1>
-<p>Halo, saya <?php echo $data['name'] ?> dan saya adalah seorang <?php echo $data['job'] ?></p>
+<div class="container">
+    <h1>About Me</h1>
+    <img src="<?= BASEURL ?>/img/profile.jpeg" alt="foto profile" class="rounded-circle" style="width: 200px; height: 200px; object-fit: cover;">
+    <p>Halo, saya <?php echo $data['name'] ?> dan saya adalah seorang <?php echo $data['job'] ?></p>
+</div>  
