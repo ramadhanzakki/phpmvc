1ee3d17d0d8ba8ddede94b36f61456f9d2474618
ramadhanzakki <ramadhanzakki506@gmail.com>
2026-04-21 14:08:07 +0700

    refactor: create jumbotron on home/index

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,13 @@
-<h1>Selamat datang di website saya</h1>
+<div class="container mt-4">
+    <div class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Selamat Datang di Website Saya!</h1>
+            <p class="col-md-8 fs-4">
+            This is a simple hero unit, a simple jumbotron-style component.
+            </p>
+            <button class="btn btn-primary btn-lg" type="button">
+            Learn more
+            </button>
+        </div>
+    </div>
+</div>
